ad5cc62c26be0b2ecfee768469ff5eb1379fa433
am-User 455 <->
2025-05-26 10:34:22 +0200

    Added Script file for ddl operations. db connector insert bug fixed

---
+++ cloud_data/Scripts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test (
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    number int
+)
--- cloud_data/database_connector.py
+++ cloud_data/database_connector.py
@@ -7,7 +7,7 @@ class DatabaseError(Exception):
     pass
 
 
-def connection():
+def _connection():
 
     load_dotenv()
     db_name = os.getenv("POSTGRES_DBNAME")
@@ -16,15 +16,13 @@ def connection():
     db_host = os.getenv("POSTGRES_ENDPOINT")
     db_port = os.getenv("POSTGRES_PORT")
 
-
     try:
         dbconn = psycopg2.connect(
             dbname = db_name,
             user = db_user,
             password = db_password,
             host = db_host,
-            port = db_port
-        )
+            port = db_port)
         print(f"Database Connection to {db_name} successfully established")
         return dbconn
 
@@ -34,27 +32,40 @@ def connection():
 
 def query(statement):
 
-    conn = connection()
+    conn = _connection()
     with conn.cursor() as curs:
         curs.execute(statement)
-        return curs.fetchall()
+        fetched_data = curs.fetchall()
+    conn.close()
+    return fetched_data
 
 
-# values=() or None, slist=[(),()] or None,type= "insert"
-def insert(table, values=None, slist=None, type="insert"):
+def insert(table: str, values: [tuple] =None, slist: [list[tuple],[tuple]]=None, type: str ="insert"):
 
-    check_columns_statement= f"SELECT column_name as column_count FROM information_schema.columns WHERE table_name = '{table}' and column_name != 'id'"
-    return_value = query(check_columns_statement) #Output von query ist [(str,),(str,),...]
+    if not table or table == "":
+        return ("Please provide a 'table' when using this function")
+
+    if not values or len(values) < 1 and not slist:
+        return ("Please provide a 'value' or 'list' when using this function")
+
+    if slist and values:
+        return ("Please provide either a 'value' or a 'list' when using this function")
+
+
+
+    query_columns= f"SELECT column_name as column_count FROM information_schema.columns WHERE table_name = '{table}' and column_name != 'id' order by column_name"
+    return_value = query(query_columns) #Output of query is [(str,),(str,),...] in alphabetical order
+
 
     columns_tuple = tuple([column[0] for column in return_value])
-    columns_count = len(columns_tuple)
+    columns_count = len(columns_tuple) # number of placeholder %s to generate
     value_placeholder = f"({('%s,' * (columns_count - 1) + '%s')})"
 
     if type == "insert":
         statement = f"INSERT INTO {table} {columns_tuple} VALUES {value_placeholder}"
 
 
-    connection = connection()
+    connection = _connection()
     with connection.cursor() as cursor:
 
         if not slist and values:
@@ -64,3 +75,5 @@ def insert(table, values=None, slist=None, type="insert"):
         if type(slist) == list and len(slist) > 0:
             cursor.executemany(statement,slist)
             return cursor.statusmessage
+
+    connection.close()
